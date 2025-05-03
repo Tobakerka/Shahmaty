b4bff18218f8f7e4f2fd65cc95c2239c9ade239f
Talybov_Ruslan <RokRus@mail.ru>
2025-05-03 03:21:59 +0300

    пройдено 5 задание

--- src/Rook.java
+++ src/Rook.java
@@ -1,4 +1,49 @@
-package PACKAGE_NAME;
+public class Rook extends ChessPiece{
+    public Rook(String color) {
+        super(color);
+    }
 
-public class Rook {
+    @Override
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+
+        if (toLine < 0 || toColumn < 0 || toLine > 7 || toColumn > 7) {
+            return false;
+        }
+
+
+        if (line == toLine && column == toColumn) {
+            return false;
+        }
+
+
+        if (line == toLine) {
+
+            for (int i = Math.min(column, toColumn) + 1; i < Math.max(column, toColumn); i++) {
+                if (chessBoard.board[line][i] != null) {
+                    return false;
+                }
+            }
+            return true;
+        } else if (column == toColumn) {
+
+            for (int i = Math.min(line, toLine) + 1; i < Math.max(line, toLine); i++) {
+                if (chessBoard.board[i][column] != null) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public String getSymbol() {
+        return "R";
+    }
 }
