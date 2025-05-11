0362f36c01127ed2a4a0f98b03f8b3d9f9af72cd
Tobakerka <RokRus@mail.ru>
2025-05-11 20:01:36 +0300

    Описан класс Rock

--- src/Rook.java
+++ src/Rook.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Rook extends ChessPiece {
 
     public Rook(String color) {
@@ -14,61 +16,51 @@ public class Rook extends ChessPiece {
     @Override
     public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
 
-        if (toLine < 0 || toColumn < 0 || toLine > 7 || toColumn > 7) {
-
+        if (toLine < 0 || toLine > 7 || toColumn < 0 || toColumn > 7) {
             return false;
         }
-
 
         if (line == toLine && column == toColumn) {
-
             return false;
         }
 
-        if (line == toLine) {
+        if (line != toLine && column != toColumn) {
+            return false;
+        }
 
-            for (int i = Math.min(column, toColumn) + 1; i < Math.max(column, toColumn); i++) {
-
-                if (chessBoard.board[line][i] != null) {
-                    if (!chessBoard.board[line][i].color.equals(this.color)) {
-                        return true;
-                    } else {
+        if (chessBoard.nowPlayer.equals(this.color)) {
+            if (line < toLine) {
+                for (int i = 1; i < Math.abs(toLine - line); i++) {
+                    if (chessBoard.board[line + i][column] != null) {
+                        return false;
+                    }
+                }
+            } else if (line > toLine) {
+                for (int i = 1; i < Math.abs(line - toLine); i++) {
+                    if (chessBoard.board[line - i][column] != null) {
+                        return false;
+                    }
+                }
+            } else if (column < toColumn) {
+                for (int i = 1; i < Math.abs(toColumn - column); i++) {
+                    if (chessBoard.board[line][column + i] != null) {
+                        return false;
+                    }
+                }
+            } else {
+                for (int i = 1; i < Math.abs(column - toColumn); i++) {
+                    if (chessBoard.board[line][column - i] != null) {
                         return false;
                     }
                 }
             }
-
-        } else if (column == toColumn) {
-
-            for (int i = Math.min(line, toLine) + 1; i < Math.max(line, toLine); i++) {
-
-                if (chessBoard.board[i][column] != null) {
-                    if (!chessBoard.board[i][column].color.equals(this.color)) {
-                        this.check = false;
-                        return true;
-                    } else {
-                        return false;
-                    }
-
-
-                }
+            if (chessBoard.board[toLine][toColumn] == null || !chessBoard.board[toLine][toColumn].getColor().equals(this.color)) {
+                return true;
+            } else {
+                return false;
             }
         }
-
-        return false;
-    }
-
-    public boolean isUnderAttack(ChessBoard chessBoard, int line, int column) {
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                if (chessBoard.board[i][j] != null && !chessBoard.board[i][j].getColor().equals(this.color)) {
-                    if (chessBoard.board[i][j].canMoveToPosition(chessBoard, i, j, line, column)) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
+        return true;
     }
 
     @Override
